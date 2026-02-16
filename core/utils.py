d2894f18acef2f15779a8f646de79607820e28d0
RIFKI-NQ <rifqinf950@gmail.com>
2026-02-17 04:44:19 +0700

    refactor: move Randomizer to utils

--- core/utils.py
+++ core/utils.py
@@ -1,10 +1,13 @@
 import pandas as pd
+import numpy as np
 import yaml
-from typing import Any
+import string
+from typing import Any, Literal
 from pathlib import Path
 from abc import ABC, abstractmethod
 from core.exceptions import (ValueNotDigitError, OutOfBoundValueError, InvalidFileTypeError,
                              FileNotFoundAppError, EmptyDataError)
+strformats = Literal["uppercase", "lowercase", "mixed"]
 
 class Helper:
     @staticmethod
@@ -98,3 +101,53 @@ class YAMLFileHandler(DataIO):
     def save(self, data):
         with open(self.file_path, "w+") as file:
             yaml.safe_dump(data, file, sort_keys=False)
+            
+class Randomizer:
+    def __init__(self):
+        self.SEED = 42
+        self.rng = np.random.default_rng(self.SEED)
+        
+    def get_random_int(self,
+                       size: int = 1,
+                       min_value: int = 1,
+                       max_value: int = 2
+                       ) -> np.ndarray | int:
+        value = self.rng.integers(min_value, max_value, size)
+        if size == 1:
+            value = value[0]
+        return value
+    
+    def get_random_float(self,
+                         size: int = 1,
+                         min_value: int = 1,
+                         max_value: int = 2,
+                         round_size: int | None = None
+                         ) -> np.ndarray | float:
+        value = self.rng.uniform(min_value, max_value, size)
+        if round_size is not None:
+            value = np.round(value, round_size)
+        if size == 1:
+            value = value[0]
+        return value
+    
+    def _get_random_letters(self, strformat: strformats | None) -> str:
+        if strformat is None:
+            strformat = "mixed"
+        if strformat == "lowercase":
+            return string.ascii_lowercase
+        elif strformat == "uppercase":
+            return string.ascii_uppercase
+        elif strformat == "mixed":
+            return string.ascii_letters
+    
+    def get_random_string(self,
+                          size: int = 1,
+                          str_length: int = 1,
+                          strformat: strformats | None = None
+                          ) -> list[str] | str:
+        letters = np.array(list(self._get_random_letters(strformat)))
+        random_str = self.rng.choice(letters, size=(size, str_length))
+        value = ["".join(row) for row in random_str]
+        if size == 1:
+            value = value[0]
+        return value
--- tests/test_randomizer.py
+++ tests/test_randomizer.py
@@ -1,7 +1,7 @@
-from core.generator.generator_logic import Randomizer
+from core.utils import Randomizer
 
 def test_randomizer():
     create_random = Randomizer()
-    print(create_random.get_random_int(10, 1, 10))
-    print(create_random.get_random_float(20, 1, 10, 2))
-    print(create_random.get_random_string(10, 3, "mixed"))
+    print(create_random.get_random_int(1, 1, 10))
+    print(create_random.get_random_float(1, 1, 10, 2))
+    print(create_random.get_random_string(1, 3, "mixed"))
