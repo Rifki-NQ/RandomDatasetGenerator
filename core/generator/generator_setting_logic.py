43794639ea7207c12ef614bcb8e8f22d68b249de
RIFKI-NQ <rifqinf950@gmail.com>
2026-03-04 21:57:45 +0700

    refactor: rename _read_config to read_config

--- core/generator/generator_setting_logic.py
+++ core/generator/generator_setting_logic.py
@@ -9,13 +9,13 @@ class GeneratorSettingLogic:
         self.RANDOM_CONFIG = ["int_min", "int_max", "float_min", "float_max", "float_round",
                               "string_length", "string_type"]
         
-    def _read_config(self) -> dict[str, str | int]:
+    def read_config(self) -> dict[str, str | int]:
         if not self.yaml_file_handler.register_filepath(self.CONFIG_FILEPATH):
             raise FileNotFoundAppError(f"Error: failed to read config data: ({self.CONFIG_FILEPATH}) because the file does not exist!")
         return self.yaml_file_handler.read(format_data = False)
     
     def get_dataset_filepath(self) -> str:
-        config_data = self._read_config()
+        config_data = self.read_config()
         if not self.DATASET_FILEPATH_KEY in config_data:
             raise FilepathNotFoundError(f"Error: {self.DATASET_FILEPATH_KEY} does not exist in the config data!")
         dataset_filepath = config_data.get(self.DATASET_FILEPATH_KEY)
@@ -24,17 +24,17 @@ class GeneratorSettingLogic:
         return dataset_filepath
     
     def change_dataset_filepath(self, new_filepath: str) -> None:
-        config_data = self._read_config()
+        config_data = self.read_config()
         config_data[self.DATASET_FILEPATH_KEY] = new_filepath
         self.yaml_file_handler.save(config_data)
         
     def get_random_config(self) -> dict[str, int | str]:
-        config_data = self._read_config()
+        config_data = self.read_config()
         config_data.pop(self.DATASET_FILEPATH_KEY, None)
         return config_data
         
     def change_random_config(self, new_config: list[int | str]) -> None:
-        config_data = self._read_config()
+        config_data = self.read_config()
         new_config_data = {}
         counter = 0
         for key, value in config_data.items():
