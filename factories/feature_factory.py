827a6c159da2ba560670fadcdab1c5459cb36bbd
RIFKI-NQ <rifqinf950@gmail.com>
2026-03-22 19:25:03 +0700

    refactor: make FeatureFactory create the _Container as an object instead of inherting it

--- factories/feature_factory.py
+++ factories/feature_factory.py
@@ -4,21 +4,20 @@ from core.generator.generator_setting_cli import GeneratorSettingCLI
 from core.generator.generator_setting_logic import GeneratorSettingLogic
 from core.utils import DataIO, Randomizer
 from core.exceptions import InvalidClassNameError, InvalidMethodNameError
-from typing import Callable, Literal
+from typing import Callable, Literal, get_args
 
 valid_class_name = Literal["GeneratorCLI", "GeneratorSettingCLI"]
-VALID_CLASS_NAME = {"GeneratorCLI", "GeneratorSettingCLI"}
+VALID_CLASS_NAME = get_args(valid_class_name)
 
 class _Container:
     def __init__(self):
-        self.yaml_file_handler =DataIO.create_dataio("yaml")
-        self.csv_file_handler = DataIO.create_dataio("csv")
-        self.randomizer = Randomizer()
-        
         """
         '_feature' is used when the object is being called as a feature
         not when called by another feature
         """
+        self.yaml_file_handler =DataIO.create_dataio("yaml")
+        self.csv_file_handler = DataIO.create_dataio("csv")
+        self.randomizer = Randomizer()
         
         self.generator_setting_logic = GeneratorSettingLogic(self.yaml_file_handler)
         self.generator_setting_feature = GeneratorSettingCLI(self.generator_setting_logic)
@@ -30,16 +29,21 @@ class _Container:
         self.generator_feature = GeneratorCLI(self.generator_logic,
                                               self.generator_setting)
 
-class FeatureFactory(_Container):
+class FeatureFactory():
+    def __init__(self):
+        self.container = _Container()
+    
     def call_method(self, class_name: valid_class_name, method_name: str | None) -> Callable[[], None]:
         if class_name not in VALID_CLASS_NAME:
             raise InvalidClassNameError(f"Error: unknown class: {class_name}")
         if method_name is None:
-            raise InvalidMethodNameError(f"Error: feature not implemented yet (method is None)")
+            raise InvalidMethodNameError(f"Error: feature not implemented yet (method = None)")
+        
         class_map = {
-            "GeneratorCLI" : self.generator_feature,
-            "GeneratorSettingCLI": self.generator_setting_feature
+            "GeneratorCLI" : self.container.generator_feature,
+            "GeneratorSettingCLI": self.container.generator_setting_feature
         }
+        
         obj = class_map.get(class_name)
         try:
             return getattr(obj, method_name)
