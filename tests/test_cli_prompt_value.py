da36cdf6f74645067534687adbd727819f12cbce
RIFKI-NQ <rifqinf950@gmail.com>
2026-03-23 06:54:10 +0700

    test: fix test case for _prompt_value after the method refactor

--- tests/test_cli_prompt_value.py
+++ tests/test_cli_prompt_value.py
@@ -1,8 +1,6 @@
-from core.generator.generator_setting_cli import _BaseCLI
-cli = _BaseCLI()
+from core.generator.base_cli import BaseCLI
+cli = BaseCLI()
 input_message =  "Enter value: "
-
-#test cases for generator_setting_cli _prompt_value returned value type
 
 # -------------- test cases with positive values --------------
 
@@ -14,19 +12,19 @@ def test_prompt_value_input_int(monkeypatch):
     assert value == 50
     assert isinstance(value, int)
 
-#accepted type = int or float
-def test_prompt_value_numbers_input_int(monkeypatch):
+#accepted type = int or float, returned type = int or float (depends which type inputted first)
+def test_prompt_value_float_input_int(monkeypatch):
     input_values = iter(["word_1", "word_2", "20", "20.5"]) # int number come first
     monkeypatch.setattr("builtins.input", lambda *args: next(input_values))
-    value = cli._prompt_value("numbers", input_message)
+    value = cli._prompt_value("float", input_message)
     assert value == 20
     assert isinstance(value, int)
 
-#accepted type = int or float
-def test_prompt_value_numbers_input_float(monkeypatch):
+#accepted type = int or float, returned type = int or float (depends which type inputted first)
+def test_prompt_value_float_input_float(monkeypatch):
     input_values = iter(["word_1", "word_2", "20.5", "20"]) # float number come first
     monkeypatch.setattr("builtins.input", lambda *args: next(input_values))
-    value = cli._prompt_value("numbers", input_message)
+    value = cli._prompt_value("float", input_message)
     assert value == 20.5
     assert isinstance(value, float)
     
@@ -40,18 +38,18 @@ def test_prompt_value_input_minus_int(monkeypatch):
     assert value == -50
     assert isinstance(value, int)
 
-#accepted type = int or float
-def test_prompt_value_numbers_input_minus_int(monkeypatch):
+#accepted type = int or float, returned type = int or float (depends which type inputted first)
+def test_prompt_value_float_input_minus_int(monkeypatch):
     input_values = iter(["word_1", "word_2", "-20", "-20.5"]) # int number come first
     monkeypatch.setattr("builtins.input", lambda *args: next(input_values))
-    value = cli._prompt_value("numbers", input_message)
+    value = cli._prompt_value("float", input_message)
     assert value == -20
     assert isinstance(value, int)
 
-#accepted type = int or float
-def test_prompt_value_numbers_input_minus_float(monkeypatch):
+#accepted type = int or float, returned type = int or float (depends which type inputted first)
+def test_prompt_value_float_input_minus_float(monkeypatch):
     input_values = iter(["word_1", "word_2", "-20.5", "-20"]) # float number come first
     monkeypatch.setattr("builtins.input", lambda *args: next(input_values))
-    value = cli._prompt_value("numbers", input_message)
+    value = cli._prompt_value("float", input_message)
     assert value == -20.5
     assert isinstance(value, float)
